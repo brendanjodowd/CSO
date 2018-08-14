4e2bee361ee50022294fc217c3315c29a99c1668
brendanjodowd <brendanjodowd@gmail.com>
2018-08-14 23:08:07 +0100

    Adding database description

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Dataset of all CSO data tables.
+#'
+#' A dataset containing all of the data tables included
+#' on the CSO Statbank website.
+#'
+#' @format A data frame with many rows and 2 variables:
+#' \describe{
+#'   \item{Item}{Full name of data table}
+#'   \item{Code}{The 5 character code for this table}
+#'   ...
+#' }
+#' @source \url{https://www.cso.ie/webserviceclient/DatasetListing.aspx}
+"cso_dataset"
