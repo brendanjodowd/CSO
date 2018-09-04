9f6aec9427855a7748787d72cec259693a3013e6
brendanjodowd <brendanjodowd@gmail.com>
2018-09-04 22:23:49 +0100

    Adding background data for shape data

---
+++ R/counties.R
@@ -0,0 +1,23 @@
+#' Shapes of 26 counties in the Republic of Ireland.
+#'
+#' A database containing shapes of 26 counties in the
+#' Republic of Ireland. To be used for producing maps
+#' of Census data.
+#'
+#' This data is dervied from shapefiles created by Ordnance
+#' Survey Ireland (OSi), and taken from their Open Data
+#' Portal. The OSi has released this data under a
+#' standard Creative Commons Licence. For more information
+#' see the README file associated with this package.
+#'
+#' @format A data frame with 57,961 rows and 7 variables:
+#' \describe{
+#'   \item{long}{Longitude}
+#'   \item{lat}{Latitude}
+#'   \item{order}{Vertex order}
+#'   \item{hole}{Vertices assoicated with holes in county shapes}
+#'   \item{piece}{Numbers for different pieces of individual counties}
+#'   \item{County}{County Name. Can be matched to Census Data and certain other datasets.}
+#' }
+#' @source \url{https://data-osi.opendata.arcgis.com/}
+"counties"
